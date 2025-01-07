b7a536d4a8a5b2f63823e689a80f2d8917915a79
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-07 16:46:14 +0500

    `Update app.py with route and docstring changes`

--- app/app.py
+++ app/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, DevOps!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return "Hello, World! This is a Flask App Tier-3 Devops Project"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
