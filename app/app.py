9163d2361658429a1e869ec3e681ba248de2e14a
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-07 16:57:36 +0500

    `Added additional text to the home route of the Flask app.`

--- app/app.py
+++ app/app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a Flask App Tier-3 Devops Project !"
+    return """Hello, World! This is a Flask App Tier-3 Devops Project !
+    This project demonstrates containerization, CI/CD, and cloud deployment, showcasing strong Tier 3 DevOps skills while using free resources.
+    Github: https://github.com/muhammadharoon26/flask-app-tier3-devops-project.git
+    Made By: muhammadharoon26.vercel.app"""
 
 
 if __name__ == "__main__":
